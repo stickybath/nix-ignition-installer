2f46c6ff1bf001600410b7adc1d3ba08f4ef4501
Keith Miller <github@stickybath.com>
2019-04-17 13:25:38 -0400

    the last test worked lets try this again

--- nix-ignition-installer.sh
+++ nix-ignition-installer.sh
@@ -40,11 +40,6 @@ printf " [passed]\n"
 printf "Installing OpenJDK 7 JRE (version 1.8.0):\n"
 yum install -y java-1.8.0-openjdk
 
-# #add ignition user
-# printf "Adding ignition user.\n"
-# adduser ignition
-# passwd -d ignition
-
 #install ignition
 printf "Downloading Ignition installer:\n"
 cmd="$(curl -o ignition-installer.run ${ignitionUrl})"
@@ -55,24 +50,24 @@ chmod +x ./ignition-installer.run
 chown -R ignition:ignition /usr/local/bin/ignition
 /etc/init.d/ignition installstart
 
-# #install PostgreSQL
-# printf "Installing PostgreSQL:\n"
-# install -y postgresql-server
-# postgresql-setup initdb
-# systemctl start postgresql
-# systemctl enable postgresql
+#install PostgreSQL
+printf "Installing PostgreSQL:\n"
+install -y postgresql-server
+postgresql-setup initdb
+systemctl start postgresql
+systemctl enable postgresql
 
-# #configure PostgreSQL
-# sudo passwd postgres
-# printf "Enter postgres database user password: "
-# read -s password
-# sudo su - postgres -c "psql -d template1 -c \"ALTER USER postgres WITH PASSWORD '$password';\""
-# sudo su - postgres -c "psql -c \"CREATE DATABASE ignition OWNER postgres\""
+#configure PostgreSQL
+sudo passwd postgres
+printf "Enter postgres database user password: "
+read -s password
+sudo su - postgres -c "psql -d template1 -c \"ALTER USER postgres WITH PASSWORD '$password';\""
+sudo su - postgres -c "psql -c \"CREATE DATABASE ignition OWNER postgres\""
 
-# #prompt user to show ip address:
-# printf "Using a web browser navigate to the ip address shown below with port 8088. "
-# printf "(e.g. \"http://OCT0.OCT1.OCT2.OCT3:8088\"):\n"
-# ip addr show | grep -v "inet6" | grep -v "127.0.0.1" | grep --color=never inet 
+#prompt user to show ip address:
+printf "Using a web browser navigate to the ip address shown below with port 8088. "
+printf "(e.g. \"http://OCT0.OCT1.OCT2.OCT3:8088\"):\n"
+ip addr show | grep -v "inet6" | grep -v "127.0.0.1" | grep --color=never inet 
 
 
 #https://support.plesk.com/hc/en-us/articles/115003321434-How-to-enable-remote-access-to-PostgreSQL-server-
