06f9e1b66eaa62e171a66697860c0fd64b6dfe17
Keith Miller <github@stickybath.com>
2019-04-17 09:24:25 -0400

    same as last

--- nix-ignition-installer.sh
+++ nix-ignition-installer.sh
@@ -13,9 +13,9 @@ ignitionUrl='http://159.203.117.161/ignition-installer.run'
 
 #configure firewall
 printf "Installing firewall rules:\n"
-firewall-cmd --zone=public --permanent --add-port=8088/tcp
-firewall-cmd --zone=public --permanent --add-port=8043/tcp
-firewall-cmd --zone=public --permanent --add-port=5432/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=8088/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=8043/tcp
+sudo firewall-cmd --zone=public --permanent --add-port=5432/tcp
 
 #disable SELinux
 printf "checking if SELinux is disabled..."
@@ -24,15 +24,15 @@ if [ "$cmd" == "Enforcing" ]; then
     printf " [failed]\n"
     printf "SELinux shall be disabled and the server shall be rebooted.\n"
     printf "re-run this script after reboot completes.\n"
-    sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
+    sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
     read -p "Press [Enter] to continue."
-    reboot
+    sudo reboot
 fi
 printf " [passed]\n"
 
 #install JRE
 printf "Installing OpenJDK 7 JRE (version 1.8.0):\n"
-yum install -y java-1.8.0-openjdk
+sudo yum install -y java-1.8.0-openjdk
 
 #install ignition
 printf "Downloading Ignition installer:\n"
@@ -40,27 +40,27 @@ cmd="$(curl -o ignition-installer.run ${ignitionUrl})"
 echo $cmd
 printf "Running Ignition installer\n:"
 chmod +x ./ignition-installer.run
-./ignition-installer.run --mode unattended --prefix /usr/local/bin/ignition --serviceuser ignition --unattendedmodeui none
-chown -R ignition:ignition /usr/local/bin/ignition
-/etc/init.d/ignition installstart
+sudo ./ignition-installer.run --mode unattended --prefix /usr/local/bin/ignition --serviceuser ignition --unattendedmodeui none
+sudo chown -R ignition:ignition /usr/local/bin/ignition
+sudo /etc/init.d/ignition installstart
 
 #install PostgreSQL
 printf "Installing PostgreSQL:\n"
-yum install -y postgresql-server
-postgresql-setup initdb
-systemctl start postgresql
-systemctl enable postgresql
+sudo yum install -y postgresql-server
+sudo postgresql-setup initdb
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
 
 #configure PostgreSQL
 sudo passwd postgres
 echo "Enter postgres database user password:"
 read -s password
-su - postgres -c "psql -d template1 -c \"ALTER USER postgres WITH PASSWORD '$password';\""
-su - postgres -c "psql -c \"CREATE DATABASE ignition OWNER postgres\""
+sudo su - postgres -c "psql -d template1 -c \"ALTER USER postgres WITH PASSWORD '$password';\""
+sudo su - postgres -c "psql -c \"CREATE DATABASE ignition OWNER postgres\""
 
 #prompt user to show ip address:
 echo "Using a web browser navigate to the ip address shown below:"
-ip addr show | grep -v "inet 6" | grep -v "127.0.0.1" | grep --color=never inet 
+ip addr show | grep -v "inet6" | grep -v "127.0.0.1" | grep --color=never inet 
 
 
 #https://support.plesk.com/hc/en-us/articles/115003321434-How-to-enable-remote-access-to-PostgreSQL-server-
